faa55f7da79b398dda64e32fde9132a7fe4ae5cc
medziedbelguith <medziedbelguith@gmail.com>
2022-07-28 15:17:52 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+class Post {
+  constructor(
+    public userId: number,
+    public id: string,
+    public title: string,
+    public body: string
+  ) {}
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +15,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-promises';
+  api: string = 'https://jsonplaceholder.typicode.com/posts';
+  data = [];
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.getPosts();
+  }
+  getPosts() {
+    const promise = new Promise<void>((resolve, reject) => {
+      const apiURL = this.api;
+      this.http.get<Post[]>(apiURL).subscribe({
+        next: (res: any) => {
+          this.data = res.map((res: any) => {
+            return new Post(res.userId, res.id, res.title, res.body);
+          });
+          resolve();
+          console.log(res);
+        },
+        error: (err: any) => {
+          reject(err);
+        },
+        complete: () => {
+          console.log('complete');
+        },
+      });
+    });
+    return promise;
+  }
 }
